d28674a1a04d6c0511bd36853c060ecb48415017
Pol-Erwan <polerwankite@gmail.com>
2019-11-21 17:02:09 +0100

    17h02

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,4 +2,15 @@ class EmailController < ApplicationController
   def index
     @emails = Email.all
   end
+
+  def create
+    my_emails = Email.create(object: Faker::Book.title,Body: Faker::Lorem.paragraphs)
+    my_emails.save
+
+    respond_to do |format|
+      format.html { redirect_to books_path }
+      format.js { }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "email#index"
   resources :tasks, except: [:show]
-  resources :emails 
+  resources :email 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
